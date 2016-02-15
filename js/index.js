766c2af541a486a68000a79b338cc996ed963ef8
Kandice <kandice.levero@gmail.com>
2016-02-15 17:54:11 -0500

    Sliding exercise-finished at home

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	 $("#answer1").hide();
+    $("#question1").click(function(){
+        $("#answer1").slideDown();
+    });
+});
+
+$(document).ready(function(){
+	 $("#answer2").hide();
+    $("#question2").click(function(){
+        $("#answer2").slideDown();
+    });
+});
+
+$(document).ready(function(){
+	 $("#answer3").hide();
+    $("#question3").click(function(){
+        $("#answer3").slideDown();
+    });
+});
+
